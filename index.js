30b824043c0ca31fee15ad30d56ddf35ab35adf0
Mihai Cioban <mcioban@axway.com>
2018-11-20 11:29:35 +0200

    Added support for uploading files using Koa.

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const serve = require('koa-static');
+const koaBody = require('koa-body');
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+
 const users = require('./src/user/UserRoute');
 
 const app = new Koa();
 const router = new Router();
 
-router
-	.get('/', (ctx, next) => {
-		ctx.body = 'Hello World';
-	});
-
 router.use('/user', users.routes(), users.allowedMethods());
 
+router.post('/', (ctx, next) => {
+	const file = ctx.request.files.file;
+	const reader = fs.createReadStream(file.path);
+	const stream = fs.createWriteStream(path.join(__dirname, '/public', file.name));
+	reader.pipe(stream);
+	console.log('uploading %s -> %s', file.name, stream.path);
+
+	ctx.redirect('/');
+});
+
 app
+	.use(koaBody({multipart: true}))
+	.use(serve(path.join(__dirname, '/public')))
 	.use(router.routes())
 	.use(router.allowedMethods());
 
