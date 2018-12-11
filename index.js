b319c27804b64830258ae1add084e6cd506b7a03
Mihai Cioban <mcioban@axway.com>
2018-12-11 17:02:57 +0200

    Added sessions to app

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const serve = require('koa-static');
 const koaBody = require('koa-body');
+const session = require('koa-session');
 const path = require('path');
 
 const users = require('./src/user/UserRoute');
 const files = require('./src/file/FileRoute');
 
 const app = new Koa();
+app.keys = ['my little secret']; // To be changed on real implementation!
+
 const router = new Router();
 
 router
@@ -15,6 +18,13 @@ router
 	.use('/file', files.routes(), files.allowedMethods());
 
 app
+	.use(session(app))
+	.use(async (ctx, next) => {
+		const n = ctx.session.views || 0;
+		ctx.session.views = n + 1;
+		  
+		await next();
+	})
 	.use(koaBody({multipart: true}))
 	.use(serve(path.join(__dirname, '/public')))
 	.use(router.routes())
